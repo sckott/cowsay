97ad0d653906d873cb4f395dfb2b26672349bf75
Scott Chamberlain <myrmecocystus@gmail.com>
2015-02-05 21:36:21 -0800

    blarg remove magrittr crap

--- R/say.r
+++ R/say.r
@@ -6,15 +6,15 @@
 #'
 #' @param what (character) What do you want to say? See details.
 #' @param by (character) Type of thing, one of cow, chicken, poop, cat, facecat, bigcat, longcat,
-#' shortcat, behindcat, longtailcat, anxiouscat, grumpycat, ant, pumpkin, ghost, spider, rabbit, 
-#' pig, snowman, frog, hypnotoad, signbunny, stretchycat, fish, trilobite, shark, buffalo, 
-#' or clippy. Alternatively, use "random" to have your message spoken by a random character. 
-#' We use \code{\link{match.arg}} internally, so you can use unique parts of words that 
-#' don't conflict with others, like "g" for "ghost" because there's no other animal that 
+#' shortcat, behindcat, longtailcat, anxiouscat, grumpycat, ant, pumpkin, ghost, spider, rabbit,
+#' pig, snowman, frog, hypnotoad, signbunny, stretchycat, fish, trilobite, shark, buffalo,
+#' or clippy. Alternatively, use "random" to have your message spoken by a random character.
+#' We use \code{\link{match.arg}} internally, so you can use unique parts of words that
+#' don't conflict with others, like "g" for "ghost" because there's no other animal that
 #' starts with "g".
 #' @param type (character) One of message (default), warning, or string (returns string)
 #' @param length (integer) Length of longcat. Ignored if other animals used.
-#' @param fortune An integer specifying the row number of fortunes.data. Alternatively which can 
+#' @param fortune An integer specifying the row number of fortunes.data. Alternatively which can
 #' be a character and grep is used to try to find a suitable row.
 #' @param ... Further args passed on to \code{\link[fortunes]{fortune}}
 #'
@@ -30,15 +30,15 @@
 #'
 #' Note that if you choose \code{by='hypnotoad'} the quote is forced to be, as you could imagine,
 #' 'All Glory to the HYPNO TOAD!'. For reference see \url{http://knowyourmeme.com/memes/hypnotoad}.
-#' 
-#' Signbunny: It's not for sure known who invented signbunny, but this article  
-#' \url{http://www.vox.com/2014/9/18/6331753/sign-bunny-meme-explained} thinks they found the 
+#'
+#' Signbunny: It's not for sure known who invented signbunny, but this article
+#' \url{http://www.vox.com/2014/9/18/6331753/sign-bunny-meme-explained} thinks they found the
 #' first use in this tweet: \url{https://twitter.com/wei_bluebear/status/329101645780770817}
-#' 
+#'
 #' Trilobite: from \url{http://www.retrojunkie.com/asciiart/animals/dinos.htm}.
-#' 
-#' Note to Windows users: there are some animals (shortcat, longcat, fish, signbunny, 
-#' stretchycat, anxiouscat, longtailcat, grumpycat) that are not available 
+#'
+#' Note to Windows users: there are some animals (shortcat, longcat, fish, signbunny,
+#' stretchycat, anxiouscat, longtailcat, grumpycat) that are not available
 #' because they use non-ASCII characters that don't display properly in R on Windows.
 #'
 #' @examples
@@ -89,40 +89,36 @@
 #' say('NO!', by='grumpycat')
 #' say('WOKE UP TODAY, IT WAS TERRIBLE', by='grumpycat')
 #' say('I HAD FUN ONCE, IT WAS AWFUL', by='grumpycat')
-#' 
+#'
 #' # Fish
 #' say(by='fish')
 #'
 #' # Bunny holding a sign
 #' say(by='signbunny')
-#' 
+#'
 #' # Trilobite
 #' say(by='trilobite')
-#' 
+#'
 #' # Shark
 #' say('Q: What do you call a solitary shark\nA: A lone shark', by='shark')
-#' 
+#'
 #' # Buffalo
 #' say('Q: What do you call a single buffalo?\nA: A buffalonely', by='buffalo')
-#' 
+#'
 #' # Clippy
 #' say(fortune=59, by="clippy")
-#' 
-#' # using pipes
-#' library("magrittr")
-#' "I HAD FUN ONCE, IT WAS AWFUL" %>% say('grumpycat')
 
 say <- function(what="Hello world!", by="cat", type="message", length=18, fortune=NULL, ...){
-  
+
   if(what=="catfact"){
     what <- fromJSON('http://catfacts-api.appspot.com/api/facts?number=1')$facts
     by <- 'cat'
   }
-  
+
   who <- get_who(by, length = length)
-  
+
   if(!is.null(fortune)) what <- "fortune"
-  
+
   if(what=="time")
     what <- as.character(Sys.time())
   if(what=="fortune") {
@@ -138,7 +134,7 @@ say <- function(what="Hello world!", by="cat", type="message", length=18, fortun
   if(by=="hypnotoad"){
     what <- "All Glory to the HYPNO TOAD!"
   }
-  
+
   switch(type,
          message = message(sprintf(who, what)),
          warning = warning(sprintf(who, what)),
