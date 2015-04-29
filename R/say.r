88c40a6ad105b240e49e1aba0d061ef839c01898
Karl Broman <kbroman@gmail.com>
2015-04-28 21:59:47 -0700

    Add smallcat to say.r

--- R/say.r
+++ R/say.r
@@ -6,8 +6,8 @@
 #'
 #' @param what (character) What do you want to say? See details.
 #' @param by (character) Type of thing, one of cow, chicken, poop, cat, facecat, bigcat, longcat,
-#' shortcat, behindcat, longtailcat, anxiouscat, grumpycat, ant, pumpkin, ghost, spider, rabbit,
-#' pig, snowman, frog, hypnotoad, signbunny, stretchycat, fish, trilobite, shark, buffalo,
+#' shortcat, behindcat, longtailcat, anxiouscat, grumpycat, smallcat, ant, pumpkin, ghost, spider,
+#' rabbit, pig, snowman, frog, hypnotoad, signbunny, stretchycat, fish, trilobite, shark, buffalo,
 #' or clippy. Alternatively, use "random" to have your message spoken by a random character.
 #' We use \code{\link{match.arg}} internally, so you can use unique parts of words that
 #' don't conflict with others, like "g" for "ghost" because there's no other animal that
