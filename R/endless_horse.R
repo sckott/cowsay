7d8db919550d3e91ab1027a8c5d853032f3a7ccc
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-20 13:15:59 -0700

    endless :horse: :horse: :horse: :horse: #32

---
+++ R/endless_horse.R
@@ -0,0 +1,27 @@
+#' Endless horse
+#' 
+#' @export
+#' @param what (character) What do you want to say? See details.
+#' @param endless (logical) Should horse be enless, you better say yes. Default: TRUE
+#' @param n (integer) Number of leg segments. Default: 10
+#' @examples \dontrun{
+#' endless_horse()
+#' endless_horse(endless = FALSE)
+#' endless_horse(n = 15)
+#' }
+endless_horse <- function(what="Hello world!", endless = TRUE, n = 10) {
+  horse <- get_who("endlesshorse", NULL)
+  message(sprintf(horse, what), appendLF = FALSE)
+  if (endless) {
+    for(i in 1:n){
+      iterendless()
+    } 
+  }
+}
+
+iterendless <- function(){
+  x <- readline()
+  if (x == "") {
+    message("                    | | | |             || |", appendLF = FALSE)
+  }
+}
