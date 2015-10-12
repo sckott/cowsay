91c918fdcf42404cd26db5645931bb04f61b1ea7
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-12 16:11:01 -0700

    check if user on windows, stop with helpful message

--- R/get_who.R
+++ R/get_who.R
@@ -2,14 +2,22 @@ get_who <- function(by, length) {
 
   animals <- get_animals()
 
-  by <- match.arg(by, c(choices=names(animals), "rms", "random"))
+  if (.Platform$OS.type == "windows") {
+    ua <- names(grep("[^\\x01-\\x7f]", animals, perl = TRUE, value = TRUE))
+    if (by %in% ua) {
+      stop("If you're on Windows, you can't use:\n", 
+           paste0(ua, collapse = "\n"), call. = FALSE)
+    }
+  }
+  
+  by <- match.arg(by, c(choices = names(animals), "rms", "random"))
 
   if (by == "random") {
     who <- sample(animals, 1)
   } else if (by == "rms") {
     who <- rms
   } else if (by == "longcat") {
-    if(!length==0){
+    if (!length == 0) {
       body <- paste(rep('    |    |\n', length), collapse = "")
       body <- gsub('\n$', '', body)
       who <- sprintf(animals[by], "%s", body)
