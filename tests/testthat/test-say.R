56102361747c094967dd831abfd9521bc6bafd56
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-12 14:45:21 -0700

    remove test set that fails on windows

--- tests/testthat/test-say.R
+++ tests/testthat/test-say.R
@@ -16,13 +16,6 @@ test_that("say by works as expected", {
   expect_equal(suppressMessages(say('%s', by = "ghost", type = "string")), animals[["ghost"]])
 })
 
-test_that("say length for longcat works as expected", {
-  expect_more_than(
-    nchar(suppressMessages(say(by = "longcat", length = 10, type = "string"))), 
-    nchar(suppressMessages(say(by = "longcat", length = 3, type = "string")))
-  )
-})
-
 test_that("say fails well", {
   expect_error(say(list(4, 5)), "what has to be of length 1")
 })
