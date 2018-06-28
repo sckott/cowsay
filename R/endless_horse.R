4b1f2ed88fef5503a82b994d4ebdf198c1cf0bf8
aedobbyn <adobbs11@gmail.com>
2018-06-28 14:59:43 -0500

    almost forgot about endless horse :racehorse:

--- R/endless_horse.R
+++ R/endless_horse.R
@@ -9,17 +9,58 @@
 #' geometrically after the first iteration in order to keep the horse
 #' on screen for a while, but it \emph{will} keep going forever.  Or
 #' until you hit escape/Ctrl-C depending on your platform).
+#' @param what_color (character) A 
+#' \href{https://github.com/r-lib/crayon}{\code{crayon}}-suported text color
+#' for \code{what}.
+#' @param horse_color (character) A 
+#' \href{https://github.com/r-lib/crayon}{\code{crayon}}-suported text color
+#' for your steed.
 #' @examples \dontrun{
 #' endless_horse()
 #' endless_horse(endless = FALSE)
 #' }
-endless_horse <- function(what="Hello world!", endless = TRUE, wait = 0.5) {
+endless_horse <- function(what="Hello world!", endless = TRUE, wait = 0.5, 
+                          what_color = NULL, horse_color = NULL) {
+  
+  if (!is.null(what_color) & !is.character(what_color)) {
+    stop("what_color must be of type character", 
+         call. = FALSE)
+  }
+  
+  if (!is.null(horse_color) & !is.character(horse_color)) {
+    stop("horse_color must be of type character", 
+         call. = FALSE)
+  }
+  
+  if (!is.null(what_color)) {
+    what_color <- crayon::make_style(what_color)
+  } else {
+    what_color <- function(x) x
+  }
+  
+  if (!is.null(horse_color)) {
+    horse_color <- crayon::make_style(horse_color)
+  } else {
+    horse_color <- function(x) x
+  }
+  
+  
   horse <- get_who("endlesshorse", NULL)
-  message(sprintf(horse, what))
+  
+  what_pos_start <- 
+    regexpr('%s', horse)[1] - 1
+  
+  what_pos_end <- what_pos_start + 3
+  
+  horse <- paste0(horse_color(substr(horse, 1, what_pos_start)),
+                what_color(what),
+                horse_color(substr(horse, what_pos_end, nchar(horse))))
+  
+  message(horse)
   while (endless) {
     tryCatch(interrupt=function(e) {endless <<- FALSE}, {
       Sys.sleep(wait)
-      message("                    | | | |             || |")
+      message(horse_color("                    | | | |             || |"))
       wait <- wait * 1.1
     })
   }
