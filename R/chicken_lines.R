ffabee5887db18fb51b9e4231d783709b549a86e
aedobbyn <adobbs11@gmail.com>
2018-06-29 13:32:22 -0500

    make the longest line :rainbow:

--- R/chicken_lines.R
+++ R/chicken_lines.R
@@ -5,6 +5,12 @@ rainbow_colors <-
   c("red", "orange", "yellow",
     "green", "blue", "purple")
 
+rainbow_dict <- 
+  tibble(
+    color = rainbow_colors,
+    num = 1:length(rainbow_colors)
+  )
+
 chicken <- animals[["chicken"]]
 
 chicken_lines <- 
@@ -26,7 +32,37 @@ chicken_lines %>%
                    rainbow_colors) %>% list()
   ) 
   
-chicken_lines %>% 
+max_char <- 
+  chicken_lines %>% 
   filter(n_char == max(n_char)) %>% 
   pull(lines)
+
+
+per_group <- floor(nchar(max_char)/6)
+total <- per_group*6
+
+max_assigned <- 
+  cut(seq(25), length(rainbow_colors),
+    include.lowest = TRUE,
+    dig.lab = 0) %>%
+  as.numeric() %>% 
+  round() 
   
+
+dict <- 
+  tibble(num = max_assigned) %>% 
+  left_join(rainbow_dict) %>% 
+  mutate(
+    char = max_char %>% 
+      str_split("") %>% .[[1]]
+  )
+
+foo <- dict %>% 
+  rowwise() %>% 
+  mutate(
+    style = crayon::make_style(color) %>% list(),
+    out = style(char)
+  )
+
+foo$out %>% str_c(collapse = "") %>% cat()
+
