527783ac96322e1294bb74e511aca45cd1154a05
aedobbyn <adobbs11@gmail.com>
2018-07-07 08:52:26 -0500

    add onexit setting of warning lenght

--- R/say.r
+++ R/say.r
@@ -220,6 +220,18 @@ say <- function(what="Hello world!", by="cat",
                 color_text(substr(who, what_pos_end, nchar(who)),
                            by_color))
   
+  if (type == "warning") {
+    if (nchar(out) < 100) {
+      wl <- 100
+    } else if (nchar(out) > 8170) {
+      wl <- 8170
+    } else {
+      wl <- nchar(out) + 1
+    }
+    warn_op <- options(warning.length = wl)
+    on.exit(options(warn_op))
+  }
+  
   switch(type,
          message = message(out),
          warning = warning(out),
