aa6813bcf736e2349c054bef1d8c20ea558532f5
aedobbyn <adobbs11@gmail.com>
2018-10-07 13:05:30 -0500

    change && to ||

--- R/say.r
+++ R/say.r
@@ -135,7 +135,7 @@ say <- function(what="Hello world!", by="cat",
   what_color <- check_color(what_color)
   by_color <- check_color(by_color)
   
-  if (crayon::has_color() == FALSE && !is.null(what_color) && !is.null(by_color)) {
+  if (crayon::has_color() == FALSE && (!is.null(what_color) || !is.null(by_color))) {
     message("Colors cannot be applied in this environment :( Try using a terminal or RStudio.")
     what_color <- NULL
     by_color <- NULL
