06d72d4112f15af0af7da4f5db60ae9fceb759f1
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-08-05 08:42:35 -0700

    move check4pkg to utils.r

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-check4pkg <- function(pkg) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    stop(sprintf("The %s package is required; please install it.", pkg), call. = FALSE)
-  } else {
-    invisible(TRUE)
-  }
-}
