7509f09261fb8f19fa0243617d00fdecd990f92c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-08-05 08:42:18 -0700

    moved check4pkg from zzz.r to utils.r

--- R/utils.R
+++ R/utils.R
@@ -19,3 +19,11 @@ check_color <- function(clr) {
   }
   return(clr)
 }
+
+check4pkg <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    stop(sprintf("The %s package is required; please install it.", pkg), call. = FALSE)
+  } else {
+    invisible(TRUE)
+  }
+}
