8d68cb6f1199cc8747b9dfe4377a5239b24aece7
Scott Chamberlain <sckott@fastmail.com>
2023-10-14 06:37:44 -0700

    fix #84 use _PACKAGE approach for pkg level manual file via usethis::use_package_doc

--- R/cowsay-package.r
+++ R/cowsay-package.r
@@ -1,36 +1,8 @@
-#' Sling messages and warnings with flair
-#'
-#' If you are familiar with `cowsay` on the cli, then you know what this
-#' is, but for R.  If not, read below.  Why?  Why not?
-#'
-#' Contributors:
-#'
-#' - Amanda Dobbyn
-#' - Andreas Brandmaier
-#' - Andy Teucher
-#' - Carson Sievert
-#' - Franz-Sebastian Krah
-#' - Guangchuang Yu
-#' - Karl Broman
-#' - Kiyoko Gotanda
-#' - Lucy D'Agostino McGowan
-#' - Noam Ross
-#' - Paolo Sonego
-#' - Philipp Boersch-Supan
-#' - Rich FitzJohn
-#' - Scott Chamberlain
-#' - Thomas Leeper
-#' - Tyler Rinker
-#' - Marion Louveaux
-#'
-#' There is only one function in the cowsay package. Simplicity for the win.
-#'
-#' Say something:
-#'
-#' say('hello world!')
-#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @importFrom fortunes fortune
 #' @importFrom rmsfact rmsfact
-#' @name cowsay
-#' @docType package
+## usethis namespace: end
 NULL
